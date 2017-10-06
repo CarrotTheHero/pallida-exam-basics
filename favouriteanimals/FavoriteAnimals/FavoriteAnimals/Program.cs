b32efd1395d85456b990183c8d89fd80a33ae723
Balazs Bognar <bali.bognar@outlook.com>
2017-10-06 11:16:18 +0200

    Animals, one added but cant add more :/ and it overwrites

---
+++ favouriteanimals/FavoriteAnimals/FavoriteAnimals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FavoriteAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press G to start: ");
+            string animal = Console.ReadLine();
+            if (!string.IsNullOrEmpty(animal))
+            {
+                Console.WriteLine("Write in your favorite animals: ");
+                animal = Console.ReadLine();
+                string[] animals = { animal };
+                File.WriteAllLines("./favourites.txt", animals);
+            }
+            Console.ReadLine();
+        }
+    }
+}
